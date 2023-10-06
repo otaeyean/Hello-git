3880ea721d606b6412e37b679d99b9bb47e297bd
otaeyean <oty12125@naver.com>
2023-10-06 14:53:29 +0000

    C4: modify hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@
 	   public static void main(String[] args) {
 		  
        System.out.println("Hello, world from GIT!");
+
        System.out.println("Changes after First commit!");
+        System.out.println("Changes after pushing to remote!);
 			    }
 }
